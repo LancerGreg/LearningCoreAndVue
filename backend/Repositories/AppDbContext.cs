3dc6100e4ca9578b4ddd1c7f5dfa0005cbd4a8db
Roman <vrv2323@gmail.com>
2021-09-03 11:46:29 +0300

    Implemented DB Repository

---
+++ backend/Repositories/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Repositories
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
---
+++ backend/Repositories/AppDbRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Repositories
+{
+    public class AppDbRepository : IDisposable
+    {
+        protected AppDbContext dbContext { get; }
+
+        private bool _disposed;
+
+        protected AppDbRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                    dbContext.Dispose();
+                _disposed = true;
+            }
+        }
+
+        ~AppDbRepository() => Dispose(false);
+    }
+}
